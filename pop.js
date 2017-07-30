8263245e35d93385da9af23a72a4c351814adc78
AdithyaKrishnaK <vineetha.kadithya2001@gmail.com>
2017-07-30 19:27:28 +0530

    u

--- pop.js
+++ pop.js
@@ -1,40 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("1").addEventListener("click", openIndex(1));
-  document.getElementById("2").addEventListener("click", openIndex(2));
-    document.getElementById("3").addEventListener("click", openIndex(3));
-  document.getElementById("4").addEventListener("click", openIndex(4));
-   document.getElementById("5").addEventListener("click", openIndex(5));
-  document.getElementById("6").addEventListener("click", openIndex(6));
-    document.getElementById("7").addEventListener("click", openIndex(7));
-  document.getElementById("8").addEventListener("click", openIndex(8));
-});
+var count =0,n=0,r=0;
+window.onload = function(){loaded()};
+function loaded(){
 
-function openIndex(i) {
-    switch (i) {
-        case 1:
-            chrome.tabs.create({active: true, url: "https://www.google.co.in/?gfe_rd=cr&ei=IATFWLqUKO7x8Aeg_46wBg"});
-            break;
-        case 2:
-            chrome.tabs.create({active: true, url: "https://www.khanacademy.org/"});
-            break;
-        case 3:
-            chrome.tabs.create({active: true, url: "http://www.amazon.in/"});
-            break;
-        case 4:
-            chrome.tabs.create({active: true, url: "https://en.oxforddictionaries.com/"});
-            break;  
-        case 5:
-            chrome.tabs.create({active: true, url: "https://www.google.co.in/keep/"});
-            break;
-        case 6:
-            chrome.tabs.create({active: true, url: "https://www.codecademy.com/"});
-            break;
-        case 7:
-            chrome.tabs.create({active: true, url: "https://www.codechef.com/"});
-            break;
-        case 8:
-            chrome.tabs.create({active: true, url: "https://www.cnet.com/"});
-            break;                       
+for(var i=0;i<10;i++){
+    var uid = count.toString();
+    var container = document.createElement("div");
+    container.setAttribute("class", "holder");
+    container.setAttribute("id", uid);
+    container.setAttribute("draggable", "true");
+    /*container.setAttribute("ondragstart", "drag(event)");
+    container.setAttribute("ondragover", "dragallow(event)");
+    container.setAttribute("ondrop", "drop(event)");*/
+    document.body.appendChild(container);
+    if (n==5) {
+        n=0;
+        r=r+1;
     }
- 
+
+    var t=150+(180*r);
+    document.getElementById(uid).style.top = t.toString()+"px";
+    var l = 70+(250*n);
+    document.getElementById(uid).style.left = l.toString()+"px";
+    var logo = document.createElement("img");
+    logo.src = "google.png";
+    logo.setAttribute("class", "icon");
+    logo.setAttribute("draggable", "false");
+    document.getElementById(uid).appendChild(logo);
+
+
+    var text = document.createElement("p");
+    text.innerHTML="Google";
+    text.setAttribute("class", "text");
+    document.getElementById(uid).appendChild(text);
+    n=n+1;
+    count = count +1;
 }
+
+}
+
+$(document).ready(function(){
+    $("#settings").click(function(){
+    $("#pan").slideToggle();
+});
+   });
